4f4bb1441905d6fe0149168e618ad8f600a9219e
Poype <253186926@qq.com>
2022-05-11 22:49:37 +0800

    hdfs的其它一些API操作

--- src/test/java/com/poype/bigdata/HdfsClientTest.java
+++ src/test/java/com/poype/bigdata/HdfsClientTest.java
@@ -1,13 +1,13 @@
 package com.poype.bigdata;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 public class HdfsClientTest {
 
@@ -24,4 +24,68 @@ public class HdfsClientTest {
         // 3 关闭资源
         fs.close();
     }
+
+    @Test
+    public void testCopyToLocalFile() throws IOException, InterruptedException, URISyntaxException{
+
+        // 1 获取文件句柄
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:8020"), configuration, "atguigu");
+
+        // 2 执行下载操作
+        // boolean delSrc 指是否将原文件删除
+        // Path src 指要下载的文件路径
+        // Path dst 指将文件下载到的路径
+        // boolean useRawLocalFileSystem 是否开启文件校验
+        fs.copyToLocalFile(false, new Path("/sanguo/jinguo.txt"), new Path("jinguo.txt"), true);
+
+        // 3 关闭资源
+        fs.close();
+    }
+
+    @Test
+    public void testRename() throws IOException, InterruptedException, URISyntaxException{
+
+        // 1 获取文件句柄
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:8020"), configuration, "atguigu");
+
+        // 2 修改文件名称
+        fs.rename(new Path("/sanguo/jinguo.txt"), new Path("/sanguo/jinguo33333.txt"));
+
+        // 3 关闭资源
+        fs.close();
+    }
+
+    @Test
+    public void testListFiles() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1获取文件系统
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:8020"), configuration, "atguigu");
+
+        // 2 获取文件详情
+        RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/"), true);
+
+        while (listFiles.hasNext()) {
+            LocatedFileStatus fileStatus = listFiles.next();
+
+            System.out.println("========" + fileStatus.getPath() + "=========");
+            System.out.println(fileStatus.getPermission());
+            System.out.println(fileStatus.getOwner());
+            System.out.println(fileStatus.getGroup());
+            System.out.println(fileStatus.getLen());
+            System.out.println(fileStatus.getModificationTime());
+            System.out.println(fileStatus.getReplication());
+            System.out.println(fileStatus.getBlockSize());
+            System.out.println(fileStatus.getPath().getName());
+
+            // 获取块信息
+            BlockLocation[] blockLocations = fileStatus.getBlockLocations();
+            System.out.println(Arrays.toString(blockLocations));
+        }
+        // 3 关闭资源
+        fs.close();
+    }
+
 }
