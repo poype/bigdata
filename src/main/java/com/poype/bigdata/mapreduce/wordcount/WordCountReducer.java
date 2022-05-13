0c38f60e4a9cf1b99cdc094515dc199b3577b4dd
Poype <253186926@qq.com>
2022-05-13 16:14:36 +0800

    MapReduce案例1，在本地化运行

---
+++ src/main/java/com/poype/bigdata/mapreduce/wordcount/WordCountReducer.java
@@ -0,0 +1,27 @@
+package com.poype.bigdata.mapreduce.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    IntWritable outValue = new IntWritable();
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+
+        // 1 累加求和
+        int sum = 0;
+        for (IntWritable count : values) {
+            sum += count.get();
+        }
+
+        // 2 输出
+        outValue.set(sum);
+        context.write(key, outValue);
+    }
+}
