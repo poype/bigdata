bcaf46162ee321a2c8b0fae5c3da42300fb37332
Poype <253186926@qq.com>
2022-05-14 13:15:15 +0800

    1. CombineTextInputFormat用于小文件过多的场景，它可以将多个小文件从逻辑上规划到一个切片中，这样，多个小文件就可以交给一个MapTask处理 2. 输入的可以是一整个目录，MR框架将一起处理目录下的所有文件

--- src/main/java/com/poype/bigdata/mapreduce/wordcount/WordCountDriver.java
+++ src/main/java/com/poype/bigdata/mapreduce/wordcount/WordCountDriver.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -32,15 +33,20 @@ public class WordCountDriver {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
+        // 如果不设置InputFormat，它默认用的是TextInputFormat.class
+        job.setInputFormatClass(CombineTextInputFormat.class);
+        // 虚拟存储切片最大值设置4MB
+        CombineTextInputFormat.setMaxInputSplitSize(job, 8388608);
+
         // 设置reduce task的数量，有几个reduce task，就会对应几个结果输出文件
         // part-r-00000, part-r-00001, part-r-00002 这里reduce的结果对应这三个文件
-        job.setNumReduceTasks(3);
+        job.setNumReduceTasks(1);
 
         // 6 设置输入和输出路径
-        FileInputFormat.setInputPaths(job, new Path(args[0]));
-//        FileInputFormat.setInputPaths(job, new Path("D:\\Temp\\hello.txt"));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-//        FileOutputFormat.setOutputPath(job, new Path("D:\\Temp\\hello_output"));
+//        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileInputFormat.setInputPaths(job, new Path("D:\\Temp\\small_file"));
+//        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path("D:\\Temp\\output1"));
 
         // 7 提交job
         boolean result = job.waitForCompletion(true);
