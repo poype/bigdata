e9bcfcfbe36159a82604a818e4922c0a3f25f6d8
Poype <253186926@qq.com>
2022-05-23 21:27:02 +0800

    collect算子也属于action类型 将RDD各个partition的数据，统一收集到Driver中，形成一个list对象 这个算子，是将RDD中各个分区数据都拉取到Driver，数据量可能很大 所以用这个算子之前要对数据量大小心知肚明，结果数据集不会太大， 不然，会把Driver内存撑爆

--- src/main/java/com/poype/bigdata/spark/third/CountByKeyOperator.java
+++ src/main/java/com/poype/bigdata/spark/third/CountByKeyOperator.java
@@ -35,3 +35,12 @@ public class CountByKeyOperator {
         System.out.println(wordCount);
     }
 }
+
+
+/**
+ * collect算子也属于action类型
+ * 将RDD各个partition的数据，统一收集到Driver中，形成一个list对象
+ * 这个算子，是将RDD中各个分区数据都拉取到Driver，数据量可能很大
+ * 所以用这个算子之前要对数据量大小心知肚明，结果数据集不会太大，
+ * 不然，会把Driver内存撑爆
+ */
