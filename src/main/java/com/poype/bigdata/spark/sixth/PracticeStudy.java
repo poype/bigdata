298b7493aab57fb5d154a853196969293a86d159
Poype <253186926@qq.com>
2022-05-25 12:37:17 +0800

    求搜索关键词出现最多的5个词是哪几个
    重点是top算子的Comparator参数很有意思，它要实现Serializable接口，否则会报错。需要重点关注。

---
+++ src/main/java/com/poype/bigdata/spark/sixth/PracticeStudy.java
@@ -0,0 +1,46 @@
+package com.poype.bigdata.spark.sixth;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function2;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PracticeStudy {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf();
+        conf.setAppName("WordCount");
+        conf.setMaster("local[*]");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // 创建文件RDD, 总共有6列，分别是 搜索时间、用户ID、搜索内容、URL返回排名、用户点击顺序、用户点击URL
+        JavaRDD<String> fileRdd = sc.textFile("./src/main/resources/SogouQ.txt", 3);
+
+        JavaRDD<List<String>> lineRdd = fileRdd.map(line -> {
+            String[] lineArray = line.split("\t");
+            return Arrays.asList(lineArray);
+        });
+
+        JavaPairRDD<String, Integer> searchWordRdd = lineRdd.mapToPair(line -> new Tuple2<>(line.get(2), 1));
+
+        JavaPairRDD<String, Integer> wordCountRdd =
+                searchWordRdd.reduceByKey((Function2<Integer, Integer, Integer>) Integer::sum);
+
+        // 这里必须明确指定SerializableComparator接口，否则会报”Task not serializable“错误
+        // 原因可能是top算子会引入shuffle，所以要将对象在网络上传输。这里被传输的对象有两类：
+        // 一类是Tuple2类型的对象，Tuple2类已经实现了Serializable接口，所以没问题
+        // 另一类是实现Comparator接口的对象，这种对象虽然只是用于排序，但也会在网络上被传输，所以也要实现Serializable接口
+        // 仔细想想Comparator对象应该没必要在网络上传输，root cause还需要深入研究一下。。。。。。。
+        List<Tuple2<String, Integer>> top5World = wordCountRdd.top(5,
+                (SerializableComparator<Tuple2<String, Integer>>) (o1, o2) -> o1._2 - o2._2);
+
+        // [(scala,2310), (hadoop,2268), (博学谷,2002), (传智汇,1918), (itheima,1680)]
+        System.out.println(top5World);
+    }
+}
