1f8c666b62da3d4c0e0941e1ff63ed917abece4e
Poype <253186926@qq.com>
2022-05-26 10:06:26 +0800

    累加器  实验前准备
    RDD中一共有10个数字，期待总数是10，但却是0。原因是累加的操作是在Executor执行的，而下面的打印是在Driver中执行的

---
+++ src/main/java/com/poype/bigdata/spark/seventh/AccumulatorStudy.java
@@ -0,0 +1,57 @@
+package com.poype.bigdata.spark.seventh;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+public class AccumulatorStudy {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf();
+        conf.setAppName("WordCount");
+        conf.setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        Integer[] numArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        JavaRDD<Integer> dataRdd = sc.parallelize(Arrays.asList(numArray), 3);
+
+        final Accumulator counter = new Accumulator();
+
+        dataRdd.foreach(integer -> {
+            counter.addOne();
+            System.out.println(counter.getValue());
+        });
+
+        // RDD中一共有10个数字，期待总数是10，但却是0。原因是累加的操作是在Executor执行的，而下面的打印是在Driver中执行的
+        // counter value: 0
+        System.out.println("counter value: " + counter.getValue());
+    }
+}
+
+// Accumulator对象要从Driver传给Executor，必须要支持序列化
+class Accumulator implements Serializable {
+
+    private int value;
+
+    public void addOne() {
+        value++;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Accumulator{" +
+                "value=" + value +
+                '}';
+    }
+}
