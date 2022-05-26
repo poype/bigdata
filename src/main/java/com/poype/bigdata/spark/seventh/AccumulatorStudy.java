3e8dda7239758659b38a8efa81ddd54afcb4b461
Poype <253186926@qq.com>
2022-05-26 22:13:02 +0800

    使用Spark的Accumulator，执行分布式加法，获取到正确的结果

--- src/main/java/com/poype/bigdata/spark/seventh/AccumulatorStudy.java
+++ src/main/java/com/poype/bigdata/spark/seventh/AccumulatorStudy.java
@@ -3,6 +3,7 @@ package com.poype.bigdata.spark.seventh;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.util.LongAccumulator;
 
 import java.io.Serializable;
 import java.util.Arrays;
@@ -18,16 +19,20 @@ public class AccumulatorStudy {
         Integer[] numArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         JavaRDD<Integer> dataRdd = sc.parallelize(Arrays.asList(numArray), 3);
 
-        final Accumulator counter = new Accumulator();
+//        final Accumulator counter = new Accumulator();
+
+        // 构建一个Spark的Accumulator
+        final LongAccumulator counter = sc.sc().longAccumulator();
 
         dataRdd.foreach(integer -> {
-            counter.addOne();
-            System.out.println(counter.getValue());
+            counter.add(1L);
+            System.out.println(counter.value());
         });
 
         // RDD中一共有10个数字，期待总数是10，但却是0。原因是累加的操作是在Executor执行的，而下面的打印是在Driver中执行的
         // counter value: 0
-        System.out.println("counter value: " + counter.getValue());
+        // 换用Spark的Accumulator后，打印结果是10
+        System.out.println("counter value: " + counter.value());
     }
 }
 
