2e1655a0b475ecbf56df8e780ddf4ba7914d8666
Poype <253186926@qq.com>
2022-05-28 21:27:44 +0800

    重命名类名

--- src/main/java/com/poype/bigdata/spark/eighth/CreateDatasetFromRDD.java
+++ src/main/java/com/poype/bigdata/spark/eighth/CreateDatasetFromRDD.java
@@ -7,7 +7,7 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-public class StructTypeStudy {
+public class CreateDatasetFromRDD {
 
     public static void main(String[] args) throws AnalysisException {
 
